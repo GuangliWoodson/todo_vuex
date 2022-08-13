3a6958554c3eb9baa9a487eb8f37fbaa48cee504
guangliwoodson <guangliwoodson@outlook.com>
2022-08-13 19:50:33 +0800

    done

---
+++ src/store/module/main.js
@@ -0,0 +1,34 @@
+export default {
+  namespaced: true,
+  state: {
+    todoList: [{
+      name: "吃饭",
+      done:true
+    }]
+  },
+  mutations: {
+    // 添加
+    addTodo(state, payload) {
+      state.todoList.unshift(payload)
+    },
+    // 修改
+    changeTodo(state, index) { 
+      state.todoList[index].done = !state.todoList[index].done
+    },
+    // 删除
+    removeTodo(state, index) {
+      state.todoList.splice(index, 1)
+    },
+    // 清空已完成
+    clearDone(state) {
+      state.todoList= state.todoList.filter(item => !item.done)
+    },
+    // 修改全选
+    changeAll(state, condition) { 
+      state.todoList.forEach(element => {
+        element.done= condition
+      });
+    },
+  },
+  actions: {}
+}
